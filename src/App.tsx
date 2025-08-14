8df68c54be58a20a95b19cf486445da844c3b09e
Andrews3002 <alexangeloandrews@gmail.com>
2025-08-14 10:59:29 -0400

    wireframe for the Home Section

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ const links = ['Home', 'About', 'Topic1', 'Topic2', 'Topic3', 'Topic4'];
 
 function App() {
   return (
-    <body>
+    <>
       <nav className="navbar">
         <NavLinks links={links} />
       </nav>
-      
-    </body>
+      <div className="liveParticleBackground">
+        <div id="homeSection">
+          <h1>Project Deepsink</h1>
+          <p>Welcome to our website, below you will find information on our preposition. Each topic below should give you insight into why you should invest in our initiative</p>
+        </div>
+      </div>
+    </>
   );
 }
 
