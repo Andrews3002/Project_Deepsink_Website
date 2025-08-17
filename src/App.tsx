57d192ff0db05985817890d11d6f02f8773e4a84
Andrews3002 <alexangeloandrews@gmail.com>
2025-08-17 14:04:34 -0400

    styling the homepage for mobile and making everything responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavLinks from "./components/NavLinks";
+import './App.css';
 
 const links = ['Home', 'About Us', 'Research Topics', 'Contact Us']; 
 const topics = ['Topic 1', 'Topic 2', 'Topic 3'];
@@ -9,21 +10,40 @@ function App() {
       <nav className="navbar">
         <NavLinks links={links} topics={topics} />
       </nav>
-      <div className="liveParticleBackground">
-        <div id="homeSection">
-          <div className="homeTitleDiv">
-            <div className="homeTitleLine">
-              <h1 className="accentText">Deep</h1>
-              <h1>Below</h1>
-            </div>
-            <div className="homeTitleLine">
-              <h1>We Breathe</h1>
-              <h1 className="accentText">Free</h1>
-            </div>
+      
+      <div id="homeSection">
+        <div className="homeTitleDiv">
+          <div className="homeTitleLine">
+            <h1 className="accentText">Deep</h1>
+            <h1>Below</h1>
           </div>
-            
-          <p>Welcome to our website, below you will find information on our preposition. Each topic below should give you insight into why you should invest in our initiative</p>
+          <div className="homeTitleLine">
+            <h1>We Breathe</h1>
+            <h1 className="accentText">Free</h1>
+          </div>
         </div>
+        <p>
+          Welcome to our website, below you will find information on our
+          proposition. Each topic below should give you insight into why you
+          should invest in our initiative
+        </p>
+      </div>
+      <div id="homeSection">
+        <div className="homeTitleDiv">
+          <div className="homeTitleLine">
+            <h1 className="accentText">Deep</h1>
+            <h1>Below</h1>
+          </div>
+          <div className="homeTitleLine">
+            <h1>We Breathe</h1>
+            <h1 className="accentText">Free</h1>
+          </div>
+        </div>
+        <p>
+          Welcome to our website, below you will find information on our
+          proposition. Each topic below should give you insight into why you
+          should invest in our initiative
+        </p>
       </div>
     </>
   );
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,53 +1,80 @@
+import { useState } from "react";
+
 interface NavLinksProps {
     links: string[];
     topics: string[];
 }
 
 const addNavLink = (topics: string[], link: string, index: number) => {
-    if (link == 'Research Topics'){
+    if (link === "Research Topics") {
         return (
-            <li className="dropdownMenu" key={index}>
-                <div className="dropdownMenuDiv">
-                    <a href="#">{link}</a>
-                    <img src="/src/assets/WhiteDropdownArrow.svg" alt="Dropdown Arrow" className="dropdownArrow" />
-                </div>
-
-                <div className="dropdownContainer">
-                    <ul className="dropdownLinks">
-                        {topics.map((subLink, subIndex) => (
-                            <li className="dropdownLink" key={subIndex}>
-                                <a href="#">{subLink}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </li>
-        )
-    }
-    else{
-        return (
-            <li className="defaultLink" key={index}>
+        <li className="dropdownMenu" key={index}>
+            <div className="dropdownMenuDiv">
                 <a href="#">{link}</a>
-            </li>
-        )
-    }
-}
+                <img src="/src/assets/WhiteDropdownArrow.svg" alt="Dropdown Arrow" className="dropdownArrow"/>
+            </div>
 
-function NavLinks({ links, topics } : NavLinksProps) {
+            <div className="dropdownContainer">
+                <ul className="dropdownLinks">
+                    {topics.map((subLink, subIndex) => (
+                        <li className="dropdownLink" key={subIndex}>
+                            <a href="#">{subLink}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </li>
+        );
+    } else {
+        return (
+        <li className="defaultLink" key={index}>
+            <a href="#">{link}</a>
+        </li>
+        );
+    }
+};
+
+function NavLinks({ links, topics }: NavLinksProps) {
+    const [open, setOpen] = useState(false);
+
     return (
         <>
-        <div className="logoDiv">
-            <img src="/src/assets/Logo.svg" alt="Logo" className="logo" />
-            <div className="logoTitleDiv">
-                <p className="logoText">Project</p>
-                <p className="logoText accentText">DeepSink</p>
+            <div className="logoDiv">
+                <img src="/src/assets/Logo.svg" alt="Logo" className="logo" />
+                <div className="logoTitleDiv">
+                    <p className="logoText">Project</p>
+                    <p className="logoText accentText">DeepSink</p>
+                </div>
             </div>
-        </div>
-        <ul className="navLinks">
-            {links.map((link, index) => (
-                addNavLink(topics, link, index)
-            ))}
-        </ul>
+
+            <ul className="navLinks">
+                {links.map((link, index) => (
+                    addNavLink(topics, link, index)
+                ))}
+            </ul>
+
+            <div className="burgerMenuDiv" onClick={() => setOpen(!open)}>
+                <div id="bar1" className={`burgerBar ${open ? "active" : ""}`}></div>
+                <div id="bar2" className={`burgerBar ${open ? "active" : ""}`}></div>
+                <div id="bar3" className={`burgerBar ${open ? "active" : ""}`}></div>
+            </div>
+
+            <ul className={`mobileNavLinks ${open ? "navActive" : ""}`}>
+                {links.map((link, index) => (
+                        link !== "Research Topics" ? (
+                            <li className="mobileNavLink" key={index}>
+                                <a href="#">{link}</a>
+                            </li>
+                        ): null
+                    )
+                )}
+
+                {topics.map((subLink, subIndex) => (
+                    <li className="mobileNavLink" key={subIndex}>
+                        <a href="#">{subLink}</a>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
